d5cc711150105522d46b9929da3f0fd9fabdbec7
Brian Fling <fling@me.com>
2014-04-19 17:17:22 -0700

    fixes

--- assets/js/application.js
+++ assets/js/application.js
@@ -27,4 +27,6 @@
 
 jQuery(document).ready(function($){
 	$('.crsl-items').carousel({ overflow: true, visible: 1, itemMinWidth: 400, itemMargin: 100, itemEqualHeight:	true, speed: 1000, autoRotate: 5000 });
+
 });
+
